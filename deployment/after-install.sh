b25dc7855a0d3a5d6835109455bc60b8e46a44b3
Davide Bizzi <davide.bizzi@unguess.io>
2023-06-01 17:11:25 +0200

    ci: Add deployment script

---
+++ deployment/after-install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euo pipefail
+
+# enable and start docker service
+systemctl enable docker.service
+systemctl start docker.service
+
+# login to ecr
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 163482350712.dkr.ecr.eu-west-1.amazonaws.com
+
+# read docker image version from manifest
+DOCKER_IMAGE=device-updater
+DOCKER_COMPOSE_FILE="/home/ec2-user/docker-compose.yml"
+INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
+ENVIRONMENT=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=environment"  --output=text | cut -f5)
+
+# pull docker image from ecr
+docker pull 163482350712.dkr.ecr.eu-west-1.amazonaws.com/$DOCKER_IMAGE
+
+# get env variables from parameter store
+mkdir -p /home/ec2-user/$APPLICATION_NAME
+aws ssm get-parameter --region eu-west-1 --name "/tryber/devices/$ENVIRONMENT/.env" --with-decryption --query "Parameter.Value" | sed -e 's/\\n/\n/g' -e 's/\\"/"/g' -e 's/^"//' -e 's/"$//' > /var/docker/.env
+
+export $(grep -v '^#' /var/docker/.env | xargs)
+
+if test -f "$DOCKER_COMPOSE_FILE"; then
+    set +e
+    IS_RUNNING=$(docker ps -a | grep $DOCKER_IMAGE| wc -l)
+    set -e
+    if [ "$IS_RUNNING" -eq "1" ]; then
+        docker-compose -f $DOCKER_COMPOSE_FILE down
+    fi
+fi
+
+echo "
+version: '3'
+services:
+  app:
+    image: 163482350712.dkr.ecr.eu-west-1.amazonaws.com/$DOCKER_IMAGE
+    restart: always
+    environment:
+      API_KEY: ${API_KEY}
+      DB_HOST: ${DB_HOST}
+      DB_PORT: ${DB_PORT}
+      DB_USER: ${DB_USER}
+      DB_PASSWORD: ${DB_PASSWORD}
+      DB_NAME: ${DB_NAME}
+      PHONEARENA_KEY: ${PHONEARENA_KEY}
+
+" > $DOCKER_COMPOSE_FILE
+
+
+docker-compose -f $DOCKER_COMPOSE_FILE up -d
